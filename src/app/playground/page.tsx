0dd4dd3aee9e16755d01ce53383a82eb9c9c7af0
lulzshadowwalker <lulzshadowwalker@gmail.com>
2025-04-23 11:42:52 +0300

    Added an interactive grid to the hero section in the home page

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,46 +1,55 @@
 'use client'
 
 import { useStats } from "@/lib/context/stats-context"
+import { InteractiveGrid } from "@/shared/interactive-grid";
+import React from 'react';
+
 
 export default function Playground() {
   const { stats } = useStats();
   console.log(stats);
 
-    const variants = [
-        'primary',
-        'primary-content',
-        'secondary',
-        'success',
-        'info',
-        'warning',
-        'danger',
-    ]
+  const variants = [
+    'primary',
+    'primary-content',
+    'secondary',
+    'success',
+    'info',
+    'warning',
+    'danger',
+  ]
 
-    return (
-        <main className="min-h-screen p-8 bg-gray-100 space-y-8">
-            <h1 className="text-2xl font-bold mb-4">Button Playground</h1>
+  return (
+    <main className="min-h-screen p-8 bg-gray-200 space-y-8">
+      <h1 className="text-2xl font-bold mb-4">Button Playground</h1>
 
-            {variants.map((variant) => (
-                <div key={variant}>
-                    <h2 className="text-lg font-semibold capitalize mb-2">
-                        {variant} buttons
-                    </h2>
-                    <div className="flex flex-wrap gap-4">
-                        <button
-                            className={`btn btn-${variant}`}
-                            onClick={() => console.log('Clicked')}
-                        >
-                            .btn .btn-{variant}
-                        </button>
-                        <button
-                            className={`btn btn-outline btn-${variant}`}
-                            onClick={() => console.log('Clicked')}
-                        >
-                            .btn .btn-outline .btn-{variant}
-                        </button>
-                    </div>
-                </div>
-            ))}
-        </main>
-    )
+      {variants.map((variant) => (
+        <div key={variant}>
+          <h2 className="text-lg font-semibold capitalize mb-2">
+            {variant} buttons
+          </h2>
+          <div className="flex flex-wrap gap-4">
+            <button
+              className={`btn btn-${variant}`}
+              onClick={() => console.log('Clicked')}
+            >
+              .btn .btn-{variant}
+            </button>
+            <button
+              className={`btn btn-outline btn-${variant}`}
+              onClick={() => console.log('Clicked')}
+            >
+              .btn .btn-outline .btn-{variant}
+            </button>
+          </div>
+        </div>
+      ))}
+
+      <h1 className="text-2xl font-bold mb-4">Boxes Grid</h1>
+      <div className="min-h-screen relative">
+        <InteractiveGrid />
+      </div>
+    </main>
+  )
 }
+
