eca4a094c0f744e4a4dadc2615b8a1f3e68f4c36
lulzie <lulzie@lulzies-MacBook-Air.local>
2025-04-29 09:36:50 +0300

    Added translations to the ios download guide

--- src/shared/ios-download-guide/index.tsx
+++ src/shared/ios-download-guide/index.tsx
@@ -6,6 +6,7 @@ import { HTMLAttributes, useRef } from "react"
 import "plyr-react/plyr.css"
 import { cn } from "@/lib/cn"
 import dynamic from "next/dynamic"
+import { useTranslations } from 'next-intl'
 
 const Plyr = dynamic(() => import("plyr-react"), {
   ssr: false,
@@ -15,63 +16,67 @@ export function IosDownloadGuide({
   className,
   ...rest
 }: HTMLAttributes<HTMLDivElement>) {
+  const t = useTranslations('ios-download-guide')
   const ref = useRef<HTMLDialogElement>(null)
   const playerRef = useRef<any>(null)
 
   function open() {
-    if (ref.current) ref.current.showModal()
+    ref.current?.showModal()
   }
 
   function handleClose() {
-    playerRef.current.plyr.stop();
+    playerRef.current.plyr.stop()
   }
 
   return (
     <>
-      <div className={cn("flex items-center gap-2 text-sm text-neutral-500 mt-4 leading-[10px] cursor-pointer relative", className)} onClick={open} {...rest}>
+      <div
+        className={cn(
+          "flex items-center gap-2 text-sm text-neutral-500 mt-4 leading-[10px] cursor-pointer relative",
+          className
+        )}
+        onClick={open}
+        {...rest}
+      >
         <div className="w-3 h-3 rounded-full bg-neutral-500/40 absolute start-0 -z-10 animate-ping duration-[4s]"></div>
         <FontAwesomeIcon icon={faInfoCircle} size="sm" />
-        how to download on iOS?
+        {t('trigger')}
       </div>
 
       <dialog className="modal modal-center" ref={ref} onClose={handleClose}>
         <div className="modal-box">
           <form method="dialog">
-            <button className="modal-close btn">
+            <button className="modal-close btn" aria-label={t('close-button')}>
               <FontAwesomeIcon icon={faXmark} size="lg" />
             </button>
           </form>
 
-          <h3 className="text-lg font-bold">Ios Download Guide</h3>
-          <p className="py-4">Watch video below</p>
+          <h3 className="text-lg font-bold">{t('title')}</h3>
+          <p className="py-4">{t('description')}</p>
 
-          <Plyr ref={playerRef}
+          <Plyr
+            ref={playerRef}
             source={{
               type: "video",
-              sources: [
-                {
-                  src: "/videos/ad-radius.mp4",
-                  provider: "html5",
-                },
-              ],
+              sources: [{ src: "/videos/ad-radius.mp4", provider: "html5" }]
             }}
-            options={{
-              autoplay: false,
-              muted: false,
-            }}
+            options={{ autoplay: false, muted: false }}
           />
 
           <div className="modal-action">
             <form method="dialog">
-              <button className="btn btn-primary">Got it!</button>
+              <button className="btn btn-primary">
+                {t('close-button')}
+              </button>
             </form>
           </div>
 
           <form method="dialog" className="modal-backdrop">
-            <button></button>
+            <button />
           </form>
         </div>
       </dialog>
     </>
   )
 }
+
